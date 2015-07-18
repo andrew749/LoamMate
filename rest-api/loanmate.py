7e2ed8bebe75181a654310dd381e2bfbff723096
Pritham Marupaka <mpritham@users.noreply.github.com>
2015-07-18 14:22:24 -0400

    Reinitialized

---
+++ rest-api/loanmate.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask
+from flask.ext.pymongo import PyMongo
+
+MONGO_URL = os.environ.get('MONGO_URL')
+if not MONGO_URL:
+    MONGO_URL = "mongodb://localhost:27017";
+
+app = Flask(__name__)
+mongo = PyMongo(app)
+
+@app.route("/")
+def hello():
+    return str(mongo.db.users.count())
+
+if __name__ == "__main__":
+    app.run()
