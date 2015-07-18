8c351fac77d1d958d25a6a42c5c59c584ba2d864
Pritham Marupaka <mpritham@users.noreply.github.com>
2015-07-18 15:18:19 -0400

    Not using heroku anymore

---
+++ rest-api/loanmate.py
@@ -0,0 +1,35 @@
+import pdb
+import json
+from flask import Flask
+from flask import request
+from flask.ext.pymongo import PyMongo
+
+app = Flask(__name__)
+mongo = PyMongo(app)
+
+@app.route("/")
+def hello():
+    return str(mongo.db.users.count())
+
+@app.route('/data/login/<username>')
+def login(username):
+    return json.dumps({"message": username})
+
+@app.route('data/payLoan')
+def pay_loan():
+    from = request.args.get('from')
+    to = request.args.get('to')
+    amount = request.args.get('amount')
+
+@app.route('data/requestLoan')
+def request_loan():
+    from = request.args.get('from')
+
+@app.route('data/userData/<username>')
+def get_user_data(username):
+    pass
+
+
+
+if __name__ == "__main__":
+    app.run()
